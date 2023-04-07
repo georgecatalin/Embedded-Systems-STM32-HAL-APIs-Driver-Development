b0d31881212fd83908c0ea0e376003263f2320a9
georgecatalin <george.calin@gmail.com>
2023-04-07 11:02:28 +0300

    Added USART3 Driver

---
+++ Universal Asynchronous Synchronous Receiver Transmitter Driver/HAL_USART_Polling_Driver/Inc/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ *  Created on: 7 Apr 2023
+ *  Author: George Calin
+ * 	Target Development Board: STM32 Nucleo F429ZI
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f4xx_hal.h"
+
+void uart3_init(void);
+
+#endif /* UART_H_ */
