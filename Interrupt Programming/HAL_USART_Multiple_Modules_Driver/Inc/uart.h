8972b52ed992fbb9aa0b5fd9469d806f40184466
georgecatalin <george.calin@gmail.com>
2023-04-11 18:20:47 +0300

    Add UART Interrupt Driver supporting multiple modules

---
+++ Interrupt Programming/HAL_USART_Multiple_Modules_Driver/Inc/uart.h
@@ -0,0 +1,18 @@
+/*
+ * uart.h
+ *
+ *  Created on: 7 Apr 2023
+ *  Author: George Calin
+ * 	Target Development Board: STM32 Nucleo F429ZI
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f4xx_hal.h"
+
+void uart1_init(void);
+void uart3_init(void);
+void configure_set_priority_irq(void);
+
+#endif /* UART_H_ */
