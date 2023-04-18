3690bedd4e33aab8c41aed3af0b97c7c449d047d
georgecatalin <george.calin@gmail.com>
2023-04-18 12:14:28 +0300

    Add SPI Interrupt Driver using HAL Library

---
+++ Serial Peripheral Interface SPI Driver Development/HAL_SPI_Interrupt_Driver/Inc/uart.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ *  Created on: Apr 10, 2023
+ *  Author: George Calin
+ * 	Target Development Board: STM32 Nucleo F429ZI
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f4xx_hal.h"
+
+void uart_init(void);
+
+
+#endif /* UART_H_ */
